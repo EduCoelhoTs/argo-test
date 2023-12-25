c242cc2ab7992afe1875827e04cecfb2f34f688f
Eduardo Coelho <123017019+EduCoelhoTs@users.noreply.github.com>
2023-12-25 16:30:04 +0000

    Fix: Files corrections

--- teste-argo-back/setup/settings.py
+++ teste-argo-back/setup/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_URL ='/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
